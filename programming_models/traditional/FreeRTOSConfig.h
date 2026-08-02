16debf0ad9f2efa0910c7eaa7f1d1f8c2314a48f
Phil Taylor <phil@m0vse.uk>
2026-08-02 02:59:31 +0100

    fix(freertos): enable legacy API compatibility

--- programming_models/traditional/FreeRTOSConfig.h
+++ programming_models/traditional/FreeRTOSConfig.h
@@ -50,7 +50,7 @@
 #define configUSE_QUEUE_SETS                    0
 #define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
-#define configENABLE_BACKWARD_COMPATIBILITY     0
+#define configENABLE_BACKWARD_COMPATIBILITY     1
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 0
 
 /* Memory allocation related definitions. */
